2066364a53efbea250f6f28306e78fc8d63c67ea
Anton Khadeev <khadeev@gmail.com>
2014-04-24 17:41:35 +0400

    Conversion into a class

--- flint.py
+++ flint.py
@@ -3,56 +3,17 @@
 import feedparser
 import sqlite3
 import time
-   
-# Global constants
+
 date_format = "%a, %d %b %Y %H:%M:%S"
+
+# Helper functions
+
+# Переменная, которая используется для вывода отладочной информации
 debug = True
 
-# helper functions
-
-def div_comment(fent):
-	""" 
-	Выделение ключевых полей из записи rss-флибусты. 
-	Запись имеет  вид: [nick] про [book_author]: [book_title]\n[comment] 
-	"""
-	# ret = [nick, book_author, book_title, comment]
-	ret = []
-
-	# nick
-	i = 0
-	while i < len(fent) and not fent[i:i+5] is " про ":
-	    i += 1
-	ret.append(fent[:i])
-	# print ("[0:",i,"] ",fent[:i])
-	
-	# book_author 
-	i += 5
-	j = i
-	while i < len(fent) and not fent[i:i+1] is ": ":
-	    i += 1
-	ret.append(fent[j:i])
-	# print ("[",j,":",i,"] ",fent[j:i])
-	
-	# book_title
-	i += 2
-	j = i
-	while i < len(fent) and not fent[i] is "\n" :
-	    i += 1
-	ret.append(fent[j:i])
-	# print ("[",j,":",i,"] ",fent[j:i])
-	
-	# comment
-	ret.append(fent[i+1:])
-	# print ("[",i+1,":] ",fent[i+1:])
-
-	return ret    
-
-def is_table_exist(table_list, table_name):
-    for c in table_list:
-        if c[0] is table_name:
-            return True
-    else:
-        return False
+def print_log(s):
+    if debug:
+        print (s)
 
 # time_sctruct -> string
 def date2str(date):
@@ -66,86 +27,157 @@ def str2date(str):
 def zero_time():
     return time.localtime(0)
 
-def print_log(s):
-	print (s)
-	if debug:
-	    f.write(s)
-	
-# Main code
+# Class definition
 
-if debug:
-    f = open('flint.log', "a")
-
-print_log("\n\n------------------------------------------------------------------------------\n%s\n" % date2str(time.localtime()))
-
-# База данных
-db = sqlite3.connect('flint.sqlite3')
-cursor = db.cursor()
-
-# Здесь лучше try...catch будет использовать
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='rssfeed' OR name ='lastdate'")
-table_list = cursor.fetchall()
-
-if not ('rssfeed',) in table_list:
-    print_log("Таблица rssfeed отсутствовала. Добавлена.\n")
-    cursor.execute('CREATE TABLE rssfeed (book_author, book_title, nick, comment, published, link)')
-
-if ('lastdate',) in table_list:
-    cursor.execute("SELECT lastdate FROM lastdate")
+class Flint(object):
+    """
+    Класс Flint
+    """
     
-    # Просто проверка на тот случай, если записей будет >1. Вообще не должно быть.
-    datesall = cursor.fetchall() 
-    
-    if len(datesall) > 0:
-	    dates = []
-	    for i in datesall:
-	        dates.append(str2date(i[0]))
+    def __init__(self):
+        """
+        Инициализация, проверка базы данных на наличие таблиц и их добавление при необходимости.
+        """
 
-	    dates.sort()
-	    startdate = dates[-1]
-	    # ToDo: убрать лишние записи - должна быть только одна
-    else:
-        startdate = zero_time()
-else:
-    print_log("Таблица lastdate отсутствовала. Добавлена.\n")
-    cursor.execute('CREATE TABLE lastdate (lastdate)')
-    startdate = zero_time()
+        print_log("\n\n------------------------------------------------------------------------------\n")
+        print_log("Кэп Флинт: новые впечатления с Флибусты.\n")
 
-# Парсинг ленты новостей Флибусты
-frss = feedparser.parse('http://flibusta.net/polka/show/all/rss')
+        self.rss_feed = 'http://flibusta.net/polka/show/all/rss'
+        self.debug    = True        
 
-imported_counter = 0
-lastdate = startdate
+        # Подключение к БД
+        self.db = sqlite3.connect('flint.sqlite3')
+        cursor  = self.db.cursor()
 
-# Сохранение новых записей в базу данных
-for fent in frss.entries:
-	if time.mktime(fent.published_parsed) > time.mktime(startdate):
-		# Выделение ключевых полей
-		# ret = [nick, book_author, book_title, comment]
-		ret = div_comment(fent.description)
-		
-		# Дата преобразуется в что-то вроде "Thu, 17 Apr 2014 17:41:09". В принципе, можно сразу брать строковую переменную
-		# из .published, но там немного по другому: "Thu, 17 Apr 2014 17:41:09 GMT"
-		ret.append(date2str(fent.published_parsed))
-		
-		# Ссылка на книгу на флибусте. По-хорошему, надо убираеть её в библиотеку книг.
-		ret.append(fent.link)
+		# Выборка таблиц
+		# Здесь лучше try...catch будет использовать
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='rssfeed' OR name ='down_date'")
+        table_list = cursor.fetchall()
 
-		cursor.execute("INSERT INTO rssfeed(nick, book_author, book_title, comment, published, link) values(?,?,?,?,?,?)", ret )
-		
-		imported_counter += 1
-		if fent.published_parsed > lastdate:
-		    lastdate = fent.published_parsed
+		# Таблица с записями
+        if not ('rssfeed',) in table_list:
+            print_log("Таблица rssfeed отсутствовала. Добавлена.\n")
+            cursor.execute('CREATE TABLE rssfeed (book_author, book_title, nick, comment, published, link)')
 
-print_log('Новых записей: %s.\n' % imported_counter)
+		# Таблица с датой последнего сообщения
+        if ('down_date',) in table_list:
+            cursor.execute("SELECT down_date FROM down_date")
+		    
+		    # Просто проверка на тот случай, если записей будет >1. Вообще не должно быть.
+            datesall = cursor.fetchall() 
+		    
+            if len(datesall) == 1:
+                # Из таблицы извлечена запись с меткой времени
+                self.down_date = str2date(datesall[0][0])
+                print_log("Дата последнего обновления извлечена из БД: %s.\n" % date2str(self.down_date))            
+            elif len(datesall) > 1:
+                # В таблице содержатся лишние метки времени
+                datesall_sorted = []
+                for i in datesall:
+                    datesall_sorted.append(str2date(i[0]))                
+                datesall_sorted.sort()
+                self.down_date = datesall_sorted[-1]
+                cursor.execute("DELETE FROM down_date WHERE NOT (down_date = ?)", (date2str(self.down_date),))
+            else:
+                # В таблице нет меток времени
+                self.down_date = zero_time()
+                print_log("В БД отсутствует дата последнего обновления. Взята нулевая отметка %s.\n" % date2str(self.down_date))		    
+                cursor.execute("INSERT INTO down_date(down_date) values(?)", (date2str(self.down_date),))
+        else:
+            cursor.execute('CREATE TABLE down_date (down_date)')
+            self.down_date = zero_time()
+            print_log("Таблица down_date отсутствовала. Добавлена.\n")
+            print_log("Взята нулевая отметка %s.\n" % date2str(self.down_date))
+            cursor.execute("INSERT INTO down_date(down_date) values(?)", (date2str(self.down_date),))
 
-if imported_counter > 0:
-    # cursor.execute('DROP TABLE lastdate')
-    cursor.execute('INSERT INTO lastdate(lastdate) values(?)',[date2str(lastdate)])
+    def __repr__(self):
+	    """
+	    """ 
+	    return date2str(self.down_date)
 
-db.commit()
-db.close()
+    def div_comment(self, fent):
+        """ 
+        Выделение ключевых полей из записи rss-флибусты. 
+        Запись имеет  вид: [nick] про [book_author]: [book_title]\n[comment] 
+        """
+        # ret = [nick, book_author, book_title, comment
+        ret = []
+        
+        # nick
+        i = 0
+        while i < len(fent) and not fent[i:i+5] is " про ":
+            i += 1
+        ret.append(fent[:i])
+        # print ("[0:",i,"] ",fent[:i])
+        
+        # book_author 
+        i += 5
+        j = i
+        while i < len(fent) and not fent[i:i+1] is ": ":
+            i += 1
+        ret.append(fent[j:i])
+        # print ("[",j,":",i,"] ",fent[j:i])
+        
+        # book_title
+        i += 2
+        j = i
+        while i < len(fent) and not fent[i] is "\n" :
+            i += 1
+        ret.append(fent[j:i])
+        # print ("[",j,":",i,"] ",fent[j:i])
+        
+        # comment
+        ret.append(fent[i+1:])
+        # print ("[",i+1,":] ",fent[i+1:])
+        
+        return ret    
 
-if debug:
-    if not f.closed:
-        f.close()
+    def update(self):
+        """
+        Обновление записей из RSS-потока.
+        """
+        
+        cursor = self.db.cursor()
+        
+        # Парсинг ленты новостей Флибусты
+        frss = feedparser.parse(self.rss_feed)
+        
+        imported_counter = 0
+        mt_down_date = time.mktime(self.down_date)
+        
+        # Сохранение новых записей в базу данных
+        for fent in frss.entries:
+            if time.mktime(fent.published_parsed) > mt_down_date:
+                # Выделение ключевых полей
+                # ret = [nick, book_author, book_title, comment]
+                ret = self.div_comment(fent.description)
+                
+                # Дата преобразуется в что-то вроде "Thu, 17 Apr 2014 17:41:09". В принципе, можно сразу брать строковую переменную
+                # из .published, но там немного по другому: "Thu, 17 Apr 2014 17:41:09 GMT"
+                ret.append(date2str(fent.published_parsed))
+                
+                # Ссылка на книгу на флибусте. По-хорошему, надо убираеть её в библиотеку книг.
+                ret.append(fent.link)
+                
+                cursor.execute("INSERT INTO rssfeed(nick, book_author, book_title, comment, published, link) values(?,?,?,?,?,?)", ret )
+                
+                if fent.published_parsed > self.down_date:
+                    self.down_date = fent.published_parsed
+
+                imported_counter += 1
+                
+        print_log('Новых записей: %s.\n' % imported_counter)
+        
+        if imported_counter > 0:
+            # cursor.execute('DROP TABLE lastdate')
+            cursor.execute('UPDATE down_date SET down_date = ?',[date2str(self.down_date)])
+
+        self.db.commit()
+
+    def close(self):
+        self.db.close()
+
+# Основная процедура
+f = Flint()
+f.update()
+f.close()
