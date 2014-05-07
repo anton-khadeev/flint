ae106ad015035e4cddbafe1327e1f332abdac49e
Anton Khadeev <khadeev@gmail.com>
2014-05-07 16:21:33 +0400

    Flint(), Hold() classes. Regexp.

--- flint.py
+++ flint.py
@@ -1,183 +1,198 @@
-# перед запуском в окне дос поменяй кодировку: chcp 1251
+# -*- coding: utf-8 -*-
+
+# Замечания: перед запуском в окне дос поменяй кодировку: chcp 1251
 
 import feedparser
 import sqlite3
 import time
-
-date_format = "%a, %d %b %Y %H:%M:%S"
+import re
 
 # Helper functions
 
-# Переменная, которая используется для вывода отладочной информации
-debug = True
+date_format = "%d %b %Y %H:%M:%S"   # predefined format
 
-def print_log(s):
-    if debug:
-        print (s)
-
-# time_sctruct -> string
 def date2str(date):
-	return time.strftime(date_format,date)
+    """ Convert 'time_sctruct' -> 'string' with predefined format """
+    return time.strftime(date_format, date)
 
-# string -> time_sctruct
 def str2date(str):
+    """ Convert 'string' -> 'time_sctruct' with predefined format """
     return time.strptime(str,date_format)
 
-# 1970/01/01 00:00:00
 def zero_time():
+    """ Starting point of machine time: 1970/01/01 00:00:00 """
     return time.localtime(0)
 
 # Class definition
 
 class Flint(object):
-    """
-    Класс Flint
-    """
+    """ Last impressions from www.flibusta.net """
+    
+    def __init__(self, water_line):
+        self.rss_feed    = 'http://flibusta.net/polka/show/all/rss'
+        self.water_line  = water_line
+        self.loot        = []
+
+    def __repr__(self):
+	    return "Flint: "+ str(len(self.loot)) + " entries, last from " + date2str(self.water_line)
+
+    def __str__(self):
+        return "Flint: "+ str(len(self.loot)) + " entries, last from " + date2str(self.water_line)
+
+    def entry_interp(self, entry):
+        """ Regexp to entry: [nick] про [book_author]: [book_title]\n[comment] """
+
+        p = re.compile(r"(?P<nick>[ .0-9a-zA-Zа-яёА-ЯЁ_-]+) про (?P<author>.+): (?P<book>.+)\n(?P<comment>(?:.|\n)*)")
+        
+        r = p.search(entry)
+        
+        if not r is None:
+            ret = dict()    # ret = [nick, book_author, book_title, comment]
+
+            ret["nick"]    = r.group("nick")
+            ret["author"]  = r.group("author")
+            ret["book"]    = r.group("book")
+            ret["comment"] = r.group("comment")
+
+            return ret    
+        else:
+            return None
+
+    def update(self):
+        """ Recieve new entries """
+        
+        frss = feedparser.parse(self.rss_feed)          # Парсинг ленты новостей Флибусты
+        
+        mt_down_date = time.mktime(self.water_line)     # Довольно неуклюжий способ сравнения двух дат,
+                                                        # надеюсь, позже придумаю лучше.       
+        for ent in frss.entries:
+            if time.mktime(ent.published_parsed) > mt_down_date:
+                ret = self.entry_interp(ent.description)
+                if not ret is None:
+
+                    # published -> '17 Apr 2014 17:41:09' (instead of 'Thu, 17 Apr 2014 17:41:09 GMT')
+                    ret["published"] = date2str(ent.published_parsed)
+                    ret["link"]      = ent.link
+
+                    self.loot.append(ret)
+
+                    if self.water_line < ent.published_parsed:
+                        self.water_line = ent.published_parsed
+                else:
+                    print ("Проблема с распознаванием записи\n'"+ent.description+'\nЗапись пропущена.')
+
+class Hold(object):
+    """ Holder for recieved entries from Flint """
     
     def __init__(self):
-        """
-        Инициализация, проверка базы данных на наличие таблиц и их добавление при необходимости.
-        """
-
-        print_log("\n\n------------------------------------------------------------------------------\n")
-        print_log("Кэп Флинт: новые впечатления с Флибусты.\n")
-
-        self.rss_feed = 'http://flibusta.net/polka/show/all/rss'
-        self.debug    = True        
-
-        # Подключение к БД
+        """ Check db on table exist """
+        
         self.db = sqlite3.connect('flint.sqlite3')
         cursor  = self.db.cursor()
 
-		# Выборка таблиц
-		# Здесь лучше try...catch будет использовать
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='rssfeed' OR name ='down_date'")
+        # Extraction of all tables
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='hold' OR name ='config'")
         table_list = cursor.fetchall()
 
-		# Таблица с записями
-        if not ('rssfeed',) in table_list:
-            print_log("Таблица rssfeed отсутствовала. Добавлена.\n")
-            cursor.execute('CREATE TABLE rssfeed (book_author, book_title, nick, comment, published, link)')
+        # Entries
+        if not ('hold',) in table_list:
+            cursor.execute('CREATE TABLE hold (nick, author, book, comment, published, link)')
+            print ('В БД отсутствовала таблица hold. Добавлена.')
 
-		# Таблица с датой последнего сообщения
-        if ('down_date',) in table_list:
-            cursor.execute("SELECT down_date FROM down_date")
-		    
-		    # Просто проверка на тот случай, если записей будет >1. Вообще не должно быть.
+        # last date
+        if not ('config',) in table_list:
+            cursor.execute('CREATE TABLE config (water_line)')
+            cursor.execute("INSERT INTO config(water_line) values(?)", (date2str(zero_time()),))
+            print ('В БД отсутствовала таблица config. Добавлена.')
+        else:
+            cursor.execute("SELECT water_line FROM config")
             datesall = cursor.fetchall() 
-		    
-            if len(datesall) == 1:
-                # Из таблицы извлечена запись с меткой времени
-                self.down_date = str2date(datesall[0][0])
-                print_log("Дата последнего обновления извлечена из БД: %s.\n" % date2str(self.down_date))            
-            elif len(datesall) > 1:
-                # В таблице содержатся лишние метки времени
+            
+            if len(datesall) > 1:
                 datesall_sorted = []
                 for i in datesall:
                     datesall_sorted.append(str2date(i[0]))                
                 datesall_sorted.sort()
-                self.down_date = datesall_sorted[-1]
-                cursor.execute("DELETE FROM down_date WHERE NOT (down_date = ?)", (date2str(self.down_date),))
-            else:
-                # В таблице нет меток времени
-                self.down_date = zero_time()
-                print_log("В БД отсутствует дата последнего обновления. Взята нулевая отметка %s.\n" % date2str(self.down_date))		    
-                cursor.execute("INSERT INTO down_date(down_date) values(?)", (date2str(self.down_date),))
-        else:
-            cursor.execute('CREATE TABLE down_date (down_date)')
-            self.down_date = zero_time()
-            print_log("Таблица down_date отсутствовала. Добавлена.\n")
-            print_log("Взята нулевая отметка %s.\n" % date2str(self.down_date))
-            cursor.execute("INSERT INTO down_date(down_date) values(?)", (date2str(self.down_date),))
-
-    def __repr__(self):
-	    """
-	    """ 
-	    return date2str(self.down_date)
-
-    def div_comment(self, fent):
-        """ 
-        Выделение ключевых полей из записи rss-флибусты. 
-        Запись имеет  вид: [nick] про [book_author]: [book_title]\n[comment] 
-        """
-        # ret = [nick, book_author, book_title, comment
-        ret = []
-        
-        # nick
-        i = 0
-        while i < len(fent) and not fent[i:i+5] is " про ":
-            i += 1
-        ret.append(fent[:i])
-        # print ("[0:",i,"] ",fent[:i])
-        
-        # book_author 
-        i += 5
-        j = i
-        while i < len(fent) and not fent[i:i+1] is ": ":
-            i += 1
-        ret.append(fent[j:i])
-        # print ("[",j,":",i,"] ",fent[j:i])
-        
-        # book_title
-        i += 2
-        j = i
-        while i < len(fent) and not fent[i] is "\n" :
-            i += 1
-        ret.append(fent[j:i])
-        # print ("[",j,":",i,"] ",fent[j:i])
-        
-        # comment
-        ret.append(fent[i+1:])
-        # print ("[",i+1,":] ",fent[i+1:])
-        
-        return ret    
-
-    def update(self):
-        """
-        Обновление записей из RSS-потока.
-        """
-        
-        cursor = self.db.cursor()
-        
-        # Парсинг ленты новостей Флибусты
-        frss = feedparser.parse(self.rss_feed)
-        
-        imported_counter = 0
-        mt_down_date = time.mktime(self.down_date)
-        
-        # Сохранение новых записей в базу данных
-        for fent in frss.entries:
-            if time.mktime(fent.published_parsed) > mt_down_date:
-                # Выделение ключевых полей
-                # ret = [nick, book_author, book_title, comment]
-                ret = self.div_comment(fent.description)
-                
-                # Дата преобразуется в что-то вроде "Thu, 17 Apr 2014 17:41:09". В принципе, можно сразу брать строковую переменную
-                # из .published, но там немного по другому: "Thu, 17 Apr 2014 17:41:09 GMT"
-                ret.append(date2str(fent.published_parsed))
-                
-                # Ссылка на книгу на флибусте. По-хорошему, надо убираеть её в библиотеку книг.
-                ret.append(fent.link)
-                
-                cursor.execute("INSERT INTO rssfeed(nick, book_author, book_title, comment, published, link) values(?,?,?,?,?,?)", ret )
-                
-                if fent.published_parsed > self.down_date:
-                    self.down_date = fent.published_parsed
-
-                imported_counter += 1
-                
-        print_log('Новых записей: %s.\n' % imported_counter)
-        
-        if imported_counter > 0:
-            # cursor.execute('DROP TABLE lastdate')
-            cursor.execute('UPDATE down_date SET down_date = ?',[date2str(self.down_date)])
+                self.water_line = datesall_sorted[-1]
+                cursor.execute("DELETE FROM config WHERE NOT (water_line = ?)", (date2str(self.water_line),))
+            elif len(datesall) == 0:
+                cursor.execute("INSERT INTO config (water_line) values(?)", (date2str(zero_time()),))
 
         self.db.commit()
 
+    def __repr__(self):
+        return "Hold: flint.sqlite3"
+
+    def __str__(self):
+        return "Hold: flint.sqlite3"
+
+    def read(self):
+        pass
+
+    def save(self, loot):
+        cursor  = self.db.cursor()
+        for e in loot:
+            cursor.execute("INSERT INTO hold (nick, author, book, comment, published, link) \
+            values(?,?,?,?,?,?)", (e["nick"], e["author"], e["book"], e["comment"], e["published"], e["link"]) )
+        self.db.commit()
+
     def close(self):
+        """ Close DB handler """
         self.db.close()
 
-# Основная процедура
-f = Flint()
-f.update()
-f.close()
+    def get_last_published(self):
+        """ Date of latest record from table. It takes from field 'published'"""       
+        cursor  = self.db.cursor()
+        cursor.execute("SELECT published FROM hold")       
+        datesall = cursor.fetchall()
+        datesall.sort()
+        return str2date(datesall[-1][0])
+
+    def get_water_line(self):
+        """ Read saved mark of last recieved data """
+        cursor  = self.db.cursor()
+        cursor.execute("SELECT water_line FROM config")
+        
+        datesall = cursor.fetchall() 
+        
+        if len(datesall) == 1:
+            return str2date(datesall[0][0])
+        elif len(datesall) > 1:
+            datesall_sorted = []
+            for i in datesall:
+                datesall_sorted.append(str2date(i[0]))                
+            datesall_sorted.sort()
+            return datesall_sorted[-1]
+        else:
+            # В таблице нет меток времени
+            return None
+
+    def update_water_line(self, water_line):
+        cursor  = self.db.cursor()
+        cursor.execute('UPDATE config SET water_line = ?',(date2str(water_line),))
+        self.db.commit()
+
+
+# Test
+
+# f = Flint(zero_time())
+# print (f)
+# print ()
+
+# f.update()
+# print (f)
+# print ()
+# print (f.loot[-1])
+# print ()
+
+# h = Hold()
+# print (h)
+# print (date2str(h.get_water_line()))
+
+# h.save(f.loot)
+
+# ld = h.get_last_published()
+# h.update_water_line(ld)
+# print(date2str(ld))
+
+# h.close()
